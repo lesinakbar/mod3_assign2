e643a21c4475d5254e814bb0f9d9d6dc56c8241a
lesinakbar <lesinakbar@gmail.com>
2016-08-05 21:00:31 +0530

    Create Assign2.java

---
+++ Assign2.java
@@ -0,0 +1,15 @@
+/*
+ * Module 3 , Assignment -2 
+ * Write a simple Java program to check whether a given number is a prime number or not.
+ */
+public class Assign2 {
+
+	public static void main(String[] args) {
+		
+		int numInt	= 98; // Enter a whole number to check whether prime or not
+		
+		Assign2a obj = new Assign2a(numInt);
+		obj.display();
+	}
+
+}
